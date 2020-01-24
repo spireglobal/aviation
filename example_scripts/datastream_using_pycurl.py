7f843dfc6c163ba4b463a9f4fae14f15c646089c
zayns-spire <56043099+zayns-spire@users.noreply.github.com>
2020-01-24 14:13:18 -0800

    Update datastream_using_pycurl.py

--- example_scripts/datastream_using_pycurl.py
+++ example_scripts/datastream_using_pycurl.py
@@ -4,7 +4,8 @@ import pycurl
 import json
 
 #Connecting to endpoint below with a 30 second timeout. 
-#Integer 30 can be changed and reflects the number of seconds the stream will run for from the time of initial connection.
+#Integer 30 in STREAM Paramaters can be changed and reflects the number of seconds the stream will run for 
+#from the time of initial connection.
 #Remember, you must replace spire-api-token with your own AirSafe Token for the request to occur successfully. 
 
 STREAM_PARAMETERS = ["https://api.airsafe.spire.com/targets", 30]
