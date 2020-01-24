005aa1968016c2d74132648c530eb1bb276b3747
zayns-spire <56043099+zayns-spire@users.noreply.github.com>
2020-01-23 18:48:08 -0800

    Update datastream_using_pycurl.py

--- example_scripts/datastream_using_pycurl.py
+++ example_scripts/datastream_using_pycurl.py
@@ -12,7 +12,7 @@ class Client:
     self.buffer = ""
     self.conn = pycurl.Curl()
     self.conn.setopt(pycurl.URL, STREAM_PARAMETERS[0])
-    self.conn.setopt(pycurl.HTTPHEADER, ['Authorization: tyCQAR7dW6h89ului0Ng2dDLL16JMFxR','Accept: application/json'])
+    self.conn.setopt(pycurl.HTTPHEADER, ['Authorization: spire-api-token','Accept: application/json'])
     self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
     self.conn.setopt(pycurl.TIMEOUT, STREAM_PARAMETERS[1])
     self.conn.perform()
