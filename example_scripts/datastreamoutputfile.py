d3617714137050c585a27def7540e8cb979df12b
zayns-spire <56043099+zayns-spire@users.noreply.github.com>
2020-02-14 11:16:50 -0800

    Update datastreamoutputfile.py

--- example_scripts/datastreamoutputfile.py
+++ example_scripts/datastreamoutputfile.py
@@ -1,15 +1,18 @@
-import csv, requests, datetime, time, json
+import csv, requests, datetime, time, json, pandas as pd
 from xml.dom import minidom
 
 #program runtime as input
 runtime = int(input('Enter program timeout, in seconds: '))
+output_csv = input('Do you want a CSV output file? (Y/N) : ')
+pp_print_stream = input('Do you want to see target_updates printed to command line during call? (Y/N) : ')
 
 #global variable for getting filename created at callstream
 FILENAME = []
+JSONDict = []
 #counter of target_updates
-target_updates_count = 0
-terrestrial_count = 0
-satellite_count = 0
+tu_count = 0
+terr_count = 0
+sat_count = 0
 
 #reads data from XML file
 class Xml:
@@ -52,44 +55,105 @@ class Stream():
             #calling datastream
             with requests.get(self.url, headers=headers, stream=True) as r:
                 with open('datastream:' + self.get_time() + '.json', 'w') as json_file:
-                    global target_updates_count
-                    global terrestrial_count
-                    global satellite_count
-                    global FILENAME
+
+                    #global declaration
+                    global tu_count
                     FILENAME.append(json_file.name)
 
                     #reading through JSON hashtables
                     for line in r.iter_lines(decode_unicode=True):
-                        target_updates_count += 1
+                        tu_count += 1
                         if time.time() < timeout:
+                            #msg is for further parsing, use loads
                             msg = json.loads(line)
+                            #dumps is for pretty print
                             json_pprint = json.dumps(msg, indent=2)
-                            print(json_pprint)
                             json_file.write(json_pprint)
+                            if pp_print_stream == 'Y':
+                                print(json_pprint)
+                            elif pp_print_stream == 'N':
+                                pass
+                            else:
+                                print(json_pprint)
 
                             #looping through dict objects and counting useful statistics for target_updates
                             data = msg['target']
-                            for key in data:
-                                if key == 'collection_type' and data[key] == 'terrestrial':
-                                    terrestrial_count += 1
-                                elif key == 'collection_type' and data[key] == 'satellite':
-                                    satellite_count += 1
+                            JSONDict.append(data)
+
                         else:
                             r.close()
         except AttributeError:
             pass
+#
+class Csv:
+
+    #attributes for class
+    def __init__(self, JSONDict):
+        self.JSONDict= JSONDict
+
+    def json2csv(self):
+        try:
+            with open('test' + '.csv', 'w', newline='') as csv_file:
+                fieldnames=['icao_address', 'timestamp', 'latitude', "longitude", "altitude_baro", "heading",
+                "ground_speed", "vertical_rate",'squawk_code' ,"on_ground", "callsign",  "tail_number", "collection_type",
+                "flight_number", "origin_airport_iata", "destination_airport_iata"]
+                csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                csv_writer.writeheader()
+                for key in self.JSONDict:
+                    for item in key:
+                        csv_writer.writerow(key)
+        except json.decoder.JSONDecodeError:
+            pass
+
+#inherit attribute from Csv
+class Statistics(Csv):
+
+
+    #attributees for class
+    def __init__(self, terrestrial, satellite, total):
+        super().__init__(JSONDict)
+        self.terrestrial = terrestrial
+        self.satellite = satellite
+        self.total = total
+
+    def statistics(self):
+
+        #global declarations for assignments
+        global terr_count
+        global sat_count
+
+        for key in self.JSONDict:
+            for item in key:
+                if item == 'collection_type' and key[item] == 'terrestrial':
+                    terr_count += 1
+                elif item == 'collection_type' and key[key] == 'satellite':
+                    sat_count += 1
 
 
 if __name__ == '__main__':
     #instance of XML file reading
     xml_arg = Xml('ds.xml', 'item')
+
     #instance of stream arguments
     xml_args_pass = xml_arg.xml_file()
-    print(xml_args_pass)
     stream_call = Stream(xml_args_pass[0], xml_args_pass[1], xml_args_pass[2])
-    print(stream_call.call_stream())
-    print('Total Target Updates: {}'.format(target_updates_count))
-    print('Terrestrial Target Updates: {}'.format(terrestrial_count))
-    print('Satellite Target Updates: {}'.format(satellite_count))
-    print('Stream Token Updates:', target_updates_count - (terrestrial_count + satellite_count))
+    stream_call.call_stream()
+
+
+    #conditional printout for CSV file
+    if output_csv == 'Y':
+        #instance of Csv
+        csv_call = Csv(JSONDict)
+        csv_call.json2csv()
+    elif output_csv == 'N':
+        pass
+
+    #instance of Statistics
+    statistics_call = Statistics(terr_count, sat_count, tu_count)
+    statistics_call.statistics()
+    #print statements for statistics
+    print('Total Target Updates: {}'.format(tu_count))
+    print('Terrestrial Target Updates: {}'.format(terr_count))
+    print('Satellite Target Updates: {}'.format(sat_count))
+    print('Stream Token Updates:', tu_count - (terr_count + sat_count))
     print('Query Paramters: {}'.format(xml_args_pass))
